f9a6bcb0774d69848c0a093b78b70f30fdca6143
Tang <2714148252@qq.com>
2023-01-04 22:13:11 +0800

    jquery

---
+++ main.js
@@ -0,0 +1,13 @@
+const x = jQuery('#test')
+    //.find('.child')
+    //.addClass('red')
+    //.end()
+    //.addClass('yellow')
+
+//x.each((div)=>console.log(div))
+
+//x.parent().print()
+
+//x.children().print()
+
+$('<div>1</div>').appendTo(document.body)
